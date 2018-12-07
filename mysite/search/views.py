68b9d3f519e36bccbb37bd3a76889436c54230f3
Unknown <tremblay.eti@gmail.com>
2018-12-07 18:35:23 -0500

    Parametric distance view

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -6,12 +6,13 @@ from .models import Seller
 from .utils import get_client_ip
 
 
-def index(request):
+def maxdist(request, max_dist_km='1'):
+    max_dist_km = float(max_dist_km)
     product_list = Product.objects.all()
     output = ""
     for p in product_list:
         distance = p.seller.get_distance(45.501691, -73.629705)
-        if distance < 2:
+        if distance < max_dist_km:
             output += ' '.join([p.type.name + " at " + p.seller.brand + ", "])
 
     return HttpResponse(output)
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,11 +5,11 @@ from django.views.generic import TemplateView
 from django_filters.views import FilterView
 
 from .search.filters import ProductFilter
-from .search.views import index
+from .search.views import maxdist
 
 
 urlpatterns = [
-    url(r'^index/$', index, name='index'),
+    url(r'^maxdist/(\d+(?:\.\d+))/$', maxdist, name='maxdist'),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^search/$', FilterView.as_view(filterset_class=ProductFilter, template_name='search/user_list.html'), name='search'),
     url(r'^admin/', include(admin.site.urls)),
